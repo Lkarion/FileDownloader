58e2dc3bb249e4a9d7a3e87375c1c2e87c10a6a9
Lkarion <97668616+Lkarion@users.noreply.github.com>
2022-03-21 21:32:28 +0100

    Update CSVFileReader.java

--- src/com/company/CSVFileReader.java
+++ src/com/company/CSVFileReader.java
@@ -3,15 +3,18 @@ package com.company;
 public class CSVFileReader {
     String linkURL;
     String action;
+    static int counter;
+    int id;
 
     public CSVFileReader(String linkURL, String action) {
         this.linkURL = linkURL;
         this.action = action;
+        id = ++counter;
     }
 
 
     @Override
     public String toString() {
-        return String.format("File: %s\nAction: %s", linkURL, action);
+        return String.format("File %s: %s\nAction %s: %s", id, linkURL, id, action);
     }
 }
